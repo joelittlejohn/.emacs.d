266b949036b8a8d0518f3a74b4c2454b849011bb
Joe Littlejohn <joelittlejohn@gmail.com>
2012-06-21 11:58:01 +0100

    Remove the 'modified buffers exist...' confirmation message

--- init.el
+++ init.el
@@ -40,3 +40,28 @@
 ;; autopair
 (require 'autopair)
 (autopair-global-mode) ;; enable autopair in all buffers
+
+;; remove the 'modified buffers exist...' confirmation message
+(defun my-save-buffers-kill-emacs (&optional arg)
+  "Offer to save each buffer(once only), then kill this Emacs process.
+With prefix ARG, silently save all file-visiting buffers, then kill."
+  (interactive "P")
+  (save-some-buffers arg t)
+  (and (or (not (fboundp 'process-list))
+       ;; process-list is not defined on MSDOS.
+       (let ((processes (process-list))
+         active)
+         (while processes
+           (and (memq (process-status (car processes)) '(run stop open listen))
+            (process-query-on-exit-flag (car processes))
+            (setq active t))
+           (setq processes (cdr processes)))
+         (or (not active)
+         (progn (list-processes t)
+            (yes-or-no-p "Active processes exist; kill them and exit anyway? ")))))
+       ;; Query the user for other things, perhaps.
+       (run-hook-with-args-until-failure 'kill-emacs-query-functions)
+       (or (null confirm-kill-emacs)
+       (funcall confirm-kill-emacs "Really exit Emacs? "))
+       (kill-emacs)))
+(fset 'save-buffers-kill-emacs 'my-save-buffers-kill-emacs)
