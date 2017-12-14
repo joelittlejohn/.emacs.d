f8d0f96dc4baf4774ea3a5060287e203449ed64b
Joe Littlejohn <joelittlejohn@gmail.com>
2017-12-14 22:22:02 +0000

    Remove bindings.el and roll custom bindings into init.el

--- init.el
+++ init.el
@@ -135,3 +135,17 @@
 (defun delete-whitespace-except-one ()
   (interactive)
   (just-one-space -1))
+
+;;; ADDITIONAL BINDINGS
+
+(global-set-key (kbd "C-x C-<left>") 'windmove-left)
+(global-set-key (kbd "C-x C-<right>") 'windmove-right)
+(global-set-key (kbd "C-x C-<up>") 'windmove-up)
+(global-set-key (kbd "C-x C-<down>") 'windmove-down)
+
+(global-set-key (kbd "M-\\") ' delete-whitespace-except-one)
+
+(require 'paredit)
+(define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
+(define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)
+(define-key paredit-mode-map (kbd "M-T") 'transpose-sexps)
